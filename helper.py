9c3517e18787a900c04f598ca2f7b8c2b7cd7f60
Haokun Lin <cameron993lin@gmail.com>
2020-05-15 01:12:34 -0700

    Finish helper interface and update gitignore file
    1. finish load_image function
    2. finish save_image function
    3. finish plot_image function
    4. update gitignore

--- helper.py
+++ helper.py
@@ -1,10 +1,31 @@
 import cv2
 import numpy as np
 
+
 class helper:
-    img = cv2.imread('dataset/vimeo_triplet/sequences/00001/0001/im1.png')
-    print(img.shape)  # (h,w,c)
-    (2, 3, 256, 448, 3)
-    def load_image(self, path, start, end):
-        
-        return 1
+    def __init__(self):
+        pass
+
+    def load_image(self, path="./dataset/vimeo_triplet/", start=1, end=1):
+        # path = "./dataset/vimeo_triplet/"
+        path += "sequences/00001/{:0>4d}/im{}.png"
+        imgs = []
+        for set_num in range(start, end):
+            set = []
+            for image_num in range(1, 4):
+                img = cv2.imread(path.format(set_num,image_num))
+                set.append(img)
+            imgs.append(set)
+        cv2.destroyAllWindows()
+        return np.asarray(imgs)
+
+    def save_image(self, path="./output/", index_start=1, images=[]):
+        filename = index_start
+        path += "{}.png"
+        for img_set in images:
+            cv2.imwrite(path.format(filename), img_set[0])
+            filename += 1
+        cv2.destroyAllWindows()
+
+    def plot_image(self,image):
+        cv2.imshow(image)
